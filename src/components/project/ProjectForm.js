10554e7c2da8bf6461c16b494c0a35a523f40fcc
Thales Euler <thaleslevs@gmail.com>
2025-01-27 10:47:49 -0300

    componentes de formulário definidos

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,23 +1,29 @@
-function ProjectForm () {
-    return (
-        <form> 
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto" />
-            </div>
-            
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            
-            <div>
-                <select name="category_id">
-                    <option disabled> Selecione a categoria </option>
-                </select>
-            </div>
+import Input from '../form/Input'
+import Select from '../form/Select'
+import Submit from '../form/Submit'
+import styles from './ProjectForm.module.css'
 
-            <div>
-                <input type="submit" value="Criar projeto" />
-            </div>
+function ProjectForm ({ btnText }) {
+    return (
+        <form className={styles.form}> 
+            
+            <Input 
+            type="text" 
+            text="Nome do projeto" 
+            name="name" 
+            placeholder="Insira o nome do projeto" 
+            />
+            
+            <Input 
+            type="number" 
+            text="Orçamento do projeto" 
+            name="budget" 
+            placeholder="Insira o orçamento total" 
+            />
+            
+            <Select name="category_id" text="Selecione a categoria" />
+
+            <Submit text={btnText} />
 
         </form>
     )
