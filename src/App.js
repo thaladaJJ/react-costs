1fb5ad70f2009a370adf60b5ba4ba6536cc121b2
Thales Euler <thaleslevs@gmail.com>
2025-01-27 06:41:26 -0300

    estrutura e estilização navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <p> Costs </p>
-    </div>
+    <Router>
+     
+      <Navbar />
+
+      <Container customClass="min-height">  
+        
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+
+          <Route path="/projects" element={<Projects />} />
+          
+          <Route path="/company" element={<Company />} />
+
+          <Route path="/contact" element={<Contact />} />
+
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,39 @@
+import {Link} from 'react-router-dom'
+
+import Container from './Container'
+
+import styles from './Navbar.module.css'
+import logo from '../../img/costs_logo.png'
+
+function Navbar () {
+
+    return (
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to="/">
+                    <img src={logo} alt="Costs" />
+                </Link>
+                <ul className={styles.list}>
+                    <li>
+                        <Link to="/"> Home </Link>
+                    </li>
+
+                    <li>
+                        <Link to="/projects"> Projetos </Link>
+                    </li>
+                    
+                    <li>
+                        <Link to="/contact"> Contact </Link>
+                    </li>
+                    
+                    <li>
+                        <Link to="/company"> Company </Link>
+                    </li>
+                </ul>
+            </Container>
+        </nav>
+    )
+
+}
+
+export default Navbar
