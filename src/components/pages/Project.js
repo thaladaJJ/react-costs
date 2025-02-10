c68a2e0c3def15cd0dea67c052f2832b8b08f197
Thales Euler <thaleslevs@gmail.com>
2025-02-10 16:19:13 -0300

    inicio da area de serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react'
 
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
+import ProjectForm from '../project/ProjectForm'
+import Message from '../layout/Message'
 
 function Project () {
     
@@ -12,6 +14,9 @@ function Project () {
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
+    const [message, setMessage] = useState()
+    const [type, setType] = useState()
 
     useEffect (() => {
         setTimeout (() => {
@@ -29,15 +34,49 @@ function Project () {
         }, 100)
     }, [id])
 
+    function editPost (project) {
+        setMessage('')
+
+        // validar o budget ()
+        if (project.budget < project.cost) {
+            setMessage('O orçamento não pode ser menor que o custo do projeto!')
+            setType('error')
+            
+            return false
+        }
+
+        fetch (`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProject (data)
+            setShowProjectForm (false)
+            
+            setMessage('Projeto atualizado!')
+            setType('sucess')
+        })
+        .catch((err) => console.log(err))
+    }
+
     function toggleProjectForm () {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm () {
+        setShowServiceForm(!showServiceForm)
     }
 
     return  (
         <> 
             {project.name ? (
             <div className={styles.project_details}> 
-                <Container customClass="column"> 
+                <Container customClass="column">
+                    {message && <Message type={type} msg={message} />}
                     <div className={styles.details_container}>
                         <h1>Projeto: {project.name}</h1>
                         <button className={styles.btn} onClick={toggleProjectForm}> 
@@ -57,9 +96,28 @@ function Project () {
                             </div>
                         ) : (
                             <div className={styles.project_info}> 
-                                <p> Forms </p> 
+                                <ProjectForm 
+                                    handleSubmit={editPost} 
+                                    btnText="Concluir edição" 
+                                    projectData={project} 
+                                />
                             </div>
                         )}
+                    </div>
+                    <div className={styles.service_form_container}>
+                        <h2>Adicione um serviço:</h2>
+                        <button className={styles.btn} onClick={toggleServiceForm}> 
+                            {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                        </button>
+                        <div className={styles.project_info}>
+                            {showServiceForm && (
+                                <div> Formulário Serviço </div>
+                            )}
+                        </div>
+                        <h2> Serviços </h2>
+                        <Container customClass="start">
+                            <p> Itens de serviços </p>
+                        </Container>
                     </div>
                 </Container>
             </div>
